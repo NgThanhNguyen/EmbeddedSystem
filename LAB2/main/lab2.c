f13960420ffb76b3c4283e813d13e7cec2d195e4
thanhnguyen <thanhhung100771@gmail.com>
2021-07-05 08:20:03 +0700

    Update lab2.c

--- LAB2/main/lab2.c
+++ LAB2/main/lab2.c
@@ -19,7 +19,7 @@
 
 #define BLINK_GPIO 17
 #define BUTTON_GPIO 18
-const TickType_t xDelay1000ms = pdMS_TO_TICKS (1000) ;
+const TickType_t xDelay1000ms = pdMS_TO_TICKS (1000);
 bool temp = false;
 
 void ATaskFunction(void *pvParameter)
@@ -28,17 +28,15 @@ void ATaskFunction(void *pvParameter)
   {
     printf("Name: Nguyen Thanh Nguyen\n");
     printf("Student ID: 1852618\n");
-    //vTaskDelay(1000 / portTICK_PERIOD_MS);
     vTaskDelay(xDelay1000ms);
-    // ...
   }
-  vTaskDelete (NULL) ;
+  vTaskDelete (NULL);
 }
 
 void ATaskblink(void *pvParameter)
 {
   {
-         /*gpio_set_level(BLINK_GPIO, 1);
+    /*gpio_set_level(BLINK_GPIO, 1);
      vTaskDelay(1000 / portTICK_PERIOD_MS);
           gpio_set_level(BLINK_GPIO, 0);
     vTaskDelay(1000 / portTICK_PERIOD_MS);*/
@@ -56,7 +54,7 @@ void ATaskbutton(void *pvParameter)
     if(gpio_get_level(BUTTON_GPIO) == 0 && temp == true) {
       if(gpio_get_level(BUTTON_GPIO) == 0){
         gpio_set_level(BLINK_GPIO, 1);
-        printf("\n\nespppppppp\n\n");
+        printf("\n\nespppppppp32\n");
         temp = false;
       }
     }
@@ -66,9 +64,7 @@ void ATaskbutton(void *pvParameter)
         temp = true;
       }
     }
-      //vTaskDelay(100 / portTICK_PERIOD_MS);
-      vTaskDelay(xDelay1000ms);
-    // ...
+    vTaskDelay(xDelay1000ms);
   }
   vTaskDelete (NULL);
   
@@ -77,27 +73,19 @@ void ATaskbutton(void *pvParameter)
 
 void app_main(void)
 {
-    static const char * pcTextForTask1 = " Task 1 is running \r\n";
-    static const char * pcTextForTask2 = " Task 2 is running \r\n";
-    
-    gpio_pad_select_gpio(BLINK_GPIO);
-    gpio_pad_select_gpio(BUTTON_GPIO);
+  gpio_pad_select_gpio(BLINK_GPIO);
+  gpio_pad_select_gpio(BUTTON_GPIO);
 
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+  gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+  gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
 
-    gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLUP_ONLY);
 
-    gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLUP_ONLY);
+  xTaskCreate(ATaskFunction,"name_task",1024,NULL,1,NULL);
 
+  //xTaskCreate(ATaskblink,"blink_task",1024,NULL,1,NULL);
 
-
-    xTaskCreate(ATaskFunction,"name_task",1024,NULL,1,NULL);
-
-    xTaskCreate(ATaskblink,"blink_task",1024,NULL,1,NULL);
-
-    xTaskCreate(ATaskbutton,"button_task",1024,NULL,1,NULL);
-
-    
+  xTaskCreate(ATaskbutton,"button_task",1024,NULL,1,NULL);    
 }
 
 
